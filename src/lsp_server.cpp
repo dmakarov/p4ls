b55e530cec118021b1df617d7ef619bf0083a8fe
Dmitri Makarov <dmakarov@spotify.com>
2018-04-01 16:16:41 +0200

    Add hunter and dependency on boost

--- src/lsp_server.cpp
+++ src/lsp_server.cpp
@@ -8,3 +8,8 @@ int LSP_server::run()
 {
     return 0;
 }
+
+boost::optional<std::string> LSP_server::read_message(std::istream &input_stream)
+{
+    return {};
+}
--- src/lsp_server.h
+++ src/lsp_server.h
@@ -1,6 +1,20 @@
+/*
+ * -*- c++ -*-
+ */
+
+#pragma once
+
+#include <istream>
+
+#include <boost/optional.hpp>
+
 class LSP_server
 {
  public:
     LSP_server();
     int run();
+
+ private:
+    boost::optional<std::string> read_message(std::istream &input_stream);
+
 };
