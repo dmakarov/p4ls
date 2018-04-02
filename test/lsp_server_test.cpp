9a75238ff8518a89f65f7ed02f39e0e017d78376
Dmitri Makarov <dmakarov@alumni.stanford.edu>
2018-04-02 21:29:34 +0200

    Enable and use boost unit test framework

---
+++ test/lsp_server_test.cpp
@@ -0,0 +1,11 @@
+#define BOOST_TEST_MODULE p4ls lsp_server
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE(lsp_server_test_suite);
+
+BOOST_AUTO_TEST_CASE(test_case1)
+{
+  BOOST_TEST(true);
+}
+
+BOOST_AUTO_TEST_SUITE_END();
