ef92016a447dcdc43f87cf4fcb35ab70bb033bc0
Dmitri Makarov <dmakarov@alumni.stanford.edu>
2018-04-05 22:33:02 +0200

    Cleanup cmake files and disable Hunter debug output

--- cmake/clang.cmake
+++ cmake/clang.cmake
@@ -1,1 +1,1 @@
-set(CMAKE_CXX_FLAGS "-Wno-variadic-macros -Wno-c99-extensions" CACHE STRING "" FORCE)
+set(CMAKE_CXX_FLAGS "-Wno-variadic-macros -Wno-c99-extensions" CACHE STRING "Suppress compiler warnings for Boost::test" FORCE)
