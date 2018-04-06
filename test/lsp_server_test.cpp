5f79b51c61302f4774c28f1d0b9b852c3cef100f
Dmitri Makarov <dmakarov@alumni.stanford.edu>
2018-04-06 19:21:31 +0200

    Add unit test driver

--- test/lsp_server_test.cpp
+++ test/lsp_server_test.cpp
@@ -1,4 +1,5 @@
-#define BOOST_TEST_MODULE p4ls lsp_server
+#include "lsp_server.h"
+
 #include <boost/test/unit_test.hpp>
 
 BOOST_AUTO_TEST_SUITE(lsp_server_test_suite);
---
+++ test/unittests_driver.cpp
@@ -0,0 +1,2 @@
+#define BOOST_TEST_MODULE p4ls unittests
+#include <boost/test/unit_test.hpp>
