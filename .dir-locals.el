2b4d9621fc363b1ee38dc48da2755ce9e68357fb
Dmitri Makarov <dmakarov@alumni.stanford.edu>
2018-04-07 21:55:59 +0200

    Enable tabs in indentation only for c/c++ mode

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,13 +1,6 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((nil
-  (indent-tabs-mode . t)
-  (tab-width . 4))
- ;(c-mode . ((c-file-style . "BSD")
- ;           (subdirs . nil)))
- ;("src/imported"
- ; . ((nil . ((change-log-default-name
- ;             . "ChangeLog.local")))))
- )
-
+((nil (tab-width . 4))
+ (c-mode . ((indent-tabs-mode . t)))
+ (c++-mode . ((indent-tabs-mode . t))))
