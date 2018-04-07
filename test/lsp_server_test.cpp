e5cfd0fbb705899092f3839f08bc33f6e9f2491c
Dmitri Makarov <dmakarov@alumni.stanford.edu>
2018-04-07 14:24:53 +0200

    Finish reformatting

--- test/lsp_server_test.cpp
+++ test/lsp_server_test.cpp
@@ -9,9 +9,9 @@ BOOST_AUTO_TEST_SUITE(lsp_server_test_suite);
 
 BOOST_AUTO_TEST_CASE(test_case1)
 {
-    std::istringstream input{"{\"project\":\"rapidjson\",\"stars\":10}"};
-    LSP_server lsp;
-    BOOST_REQUIRE_EQUAL(lsp.run(input), 0);
+	std::istringstream input{"{\"project\":\"rapidjson\",\"stars\":10}"};
+	LSP_server lsp;
+	BOOST_REQUIRE_EQUAL(lsp.run(input), 0);
 }
 
 BOOST_AUTO_TEST_SUITE_END();
