03ad50e193ca2d699d3827fe2aab49f41f12fd73
Dmitri Makarov <dmakarov@alumni.stanford.edu>
2018-04-08 21:52:09 +0200

    Use Boost::log library for logging

--- cmake/clang.cmake
+++ cmake/clang.cmake
@@ -1,1 +1,1 @@
-set(CMAKE_CXX_FLAGS "-Wno-variadic-macros -Wno-c99-extensions" CACHE STRING "Suppress compiler warnings for Boost::test" FORCE)
+set(CMAKE_CXX_FLAGS "-Wno-variadic-macros -Wno-c99-extensions -fvisibility=hidden -fvisibility-inlines-hidden" CACHE STRING "Suppress compiler warnings for Boost::test" FORCE)
--- src/tool/main.cpp
+++ src/tool/main.cpp
@@ -1,9 +1,14 @@
 #include "lsp_server.h"
 
+#include <boost/log/trivial.hpp>
+#include <boost/log/utility/setup/file.hpp>
+
 #include <iostream>
 
 int main(int argc, char* argv[])
 {
+	boost::log::add_file_log("p4lsd.log");
+	BOOST_LOG_TRIVIAL(info) << "p4lsd started";
 	LSP_server the_server;
 	return the_server.run(std::cin);
 }
