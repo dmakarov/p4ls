9a6ebf1a65639f6f7b2d82829b4754f44b3ce646
Dmitri Makarov <dmakarov@alumni.stanford.edu>
2018-04-10 20:09:18 +0200

    Enable logging to syslog

--- src/tool/main.cpp
+++ src/tool/main.cpp
@@ -1,15 +1,35 @@
+#define BOOST_LOG_USE_NATIVE_SYSLOG
+
 #include "lsp_server.h"
 
-#include <boost/log/trivial.hpp>
-#include <boost/log/utility/setup/file.hpp>
+#include <boost/log/attributes.hpp>
+#include <boost/log/common.hpp>
+#include <boost/log/expressions.hpp>
+#include <boost/log/sinks/sync_frontend.hpp>
+#include <boost/log/sinks/syslog_backend.hpp>
+#include <boost/smart_ptr/make_shared_object.hpp>
+#include <boost/smart_ptr/shared_ptr.hpp>
 // for std::cin
 #include <iostream>
+#include <memory>
 
+void init_native_syslog()
+{
+	// Create a syslog sink
+	boost::shared_ptr<boost::log::sinks::synchronous_sink<boost::log::sinks::syslog_backend>> sink(new boost::log::sinks::synchronous_sink<boost::log::sinks::syslog_backend>(boost::log::keywords::use_impl = boost::log::sinks::syslog::native, boost::log::keywords::facility = boost::log::sinks::syslog::local7));
+	sink->set_formatter(boost::log::expressions::format("%1%: %2%") % boost::log::expressions::attr<unsigned int>("RecordID") % boost::log::expressions::smessage);
+	sink->locked_backend()->set_severity_mapper(boost::log::sinks::syslog::direct_severity_mapping<int>("Severity"));
+	// Add the sink to the core
+	boost::log::core::get()->add_sink(sink);
+	// Add some attributes too
+	boost::log::core::get()->add_global_attribute("RecordID", boost::log::attributes::counter<unsigned int>());
+}
 
 int main(int argc, char* argv[])
 {
-	boost::log::add_file_log("p4lsd.log");
-	BOOST_LOG_TRIVIAL(info) << "p4lsd started";
+	init_native_syslog();
+	boost::log::sources::severity_logger<int> lg(boost::log::keywords::severity = boost::log::sinks::syslog::debug);
+	BOOST_LOG_SEV(lg, boost::log::sinks::syslog::notice) << "STARTED";
 	LSP_server the_server;
 	return the_server.run(std::cin);
 }
