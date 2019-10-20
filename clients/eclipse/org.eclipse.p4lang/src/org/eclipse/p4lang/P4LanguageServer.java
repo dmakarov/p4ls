37059e49548aa6b6d0d107318f07fe010eff7abd
Dmitri Makarov <dmakarov@alumni.stanford.edu>
2019-10-20 09:53:42 +0200

    Update eclipse client to work with more recent lsp4e

--- clients/eclipse/org.eclipse.p4lang/src/org/eclipse/p4lang/P4LanguageServer.java
+++ clients/eclipse/org.eclipse.p4lang/src/org/eclipse/p4lang/P4LanguageServer.java
@@ -1,33 +1,102 @@
 package org.eclipse.p4lang;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URI;
+import java.text.MessageFormat;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
-import org.eclipse.core.runtime.Platform;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.jface.preference.PreferenceDialog;
-import org.eclipse.lsp4e.server.ProcessStreamConnectionProvider;
+import org.eclipse.lsp4e.server.StreamConnectionProvider;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.dialogs.PreferencesUtil;
 
-public class P4LanguageServer extends ProcessStreamConnectionProvider {
+import com.google.gson.Gson;
+import com.google.gson.stream.JsonReader;
+
+public class P4LanguageServer implements StreamConnectionProvider {
 
 	private static boolean hasCancelledSetup = false;
+	private Process process;
 
-	public P4LanguageServer() {
-		List<String> commands = getP4LS();
-		if (!commands.isEmpty()) {
-			setCommands(commands);
-			setWorkingDirectory(Platform.getLocation().toOSString());
+	@Override
+	public void start() throws IOException {
+		String p4ls = getP4LS();
+		if ((p4ls.isEmpty() || !setSystemProperties())) {
+			showSetupP4Notification();
+			return;
 		}
+		this.process = P4langPlugin.getProcessForCommand(p4ls.split("[ ]+"));
 	}
 
 	@Override
 	public String toString() {
 		return "P4 Language Server: " + super.toString();
+	}
+
+	@Override
+	public Object getInitializationOptions(URI rootUri) {
+		final String settingsPath = "nofile";
+		final File settingsFile = new File(settingsPath);
+		final Gson gson = new Gson();
+		try (JsonReader reader = new JsonReader(new FileReader(settingsFile))) {
+			return gson.fromJson(reader, HashMap.class);
+		} catch (FileNotFoundException e) {
+			P4langPlugin.getDefault().getLog().log(new Status(IStatus.INFO,
+					P4langPlugin.getDefault().getBundle().getSymbolicName(),
+					MessageFormat.format("Configuration {0} not found", settingsPath)));
+		} catch (Throwable e) {
+			P4langPlugin.getDefault().getLog().log(new Status(IStatus.ERROR,
+					P4langPlugin.getDefault().getBundle().getSymbolicName(),
+					MessageFormat.format("Configuration error {0}, {1}", settingsPath, e)));
+		}
+		return getDefaultInitializationOptions();
+	}
+
+	@Override
+	public InputStream getInputStream() {
+		P4langPlugin.getDefault().getLog().log(new Status(IStatus.INFO,
+				P4langPlugin.getDefault().getBundle().getSymbolicName(),
+				MessageFormat.format("P4LSD process isAlive {0}", process.isAlive())));
+		return process == null ? null : process.getInputStream();
+	}
+
+	@Override
+	public OutputStream getOutputStream() {
+		return process == null ? null : process.getOutputStream();
+	}
+
+	@Override
+	public void stop() {
+		if (process != null)
+			process.destroy();
+	}
+
+	@Override
+	public InputStream getErrorStream() {
+		return process == null ? null : process.getErrorStream();
+	}
+
+	private static synchronized void setHasCancelledSetup(Boolean newValue) {
+		hasCancelledSetup = newValue;
+	}
+
+	private static Map<String, Object> getDefaultInitializationOptions() {
+		final Map<String, Object> initializationSettings = new HashMap<>();
+		initializationSettings.put("clippy_preference", "on"); //$NON-NLS-1$//$NON-NLS-2$
+		return Collections.singletonMap("settings", Collections.singletonMap("p4lang", initializationSettings)); //$NON-NLS-1$ //$NON-NLS-2$
 	}
 
 	private boolean setSystemProperties() {
@@ -45,8 +114,8 @@ public class P4LanguageServer extends ProcessStreamConnectionProvider {
 		 */
 	}
 
-	private List<String> getP4LS() {
-		List<String> command = new ArrayList<>();
+	private String getP4LS() {
+		String command = new String();
 		boolean wereSystemPropertiesSet = setSystemProperties();
 		P4langPlugin plugin = P4langPlugin.getDefault();
 		IPreferenceStore preferenceStore = plugin.getPreferenceStore();
@@ -56,12 +125,10 @@ public class P4LanguageServer extends ProcessStreamConnectionProvider {
 			showSetupP4Notification();
 			return command;
 		}
-		command.add(p4lsd);
+		command += p4lsd;
 		String args = preferenceStore.getString(P4langPreferenceInitializer.p4lsdArgsPreference);
 		if (!args.isEmpty()) {
-			for (final String arg : args.split("[ ]+")) {
-				command.add(arg);
-			}
+			command += " " + args;
 		}
 		return command;
 	}
@@ -81,7 +148,7 @@ public class P4LanguageServer extends ProcessStreamConnectionProvider {
 			preferenceDialog.setBlockOnOpen(true);
 			preferenceDialog.open();
 		} else {
-			hasCancelledSetup = true;
+			setHasCancelledSetup(true);
 		}
 	}
 }
--- clients/eclipse/org.eclipse.p4lang/src/org/eclipse/p4lang/P4langPlugin.java
+++ clients/eclipse/org.eclipse.p4lang/src/org/eclipse/p4lang/P4langPlugin.java
@@ -1,5 +1,9 @@
 package org.eclipse.p4lang;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.MessageFormat;
+
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.core.runtime.Status;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
@@ -44,4 +48,26 @@ public class P4langPlugin extends AbstractUIPlugin {
 	public static void logError(final String m) {
 		plugin.getLog().log(new Status(IStatus.ERROR, plugin.getBundle().getSymbolicName(), m));
 	}
+	
+	public static Process getProcessForCommand(String... commandStrings) throws IOException {
+		ProcessBuilder builder = new ProcessBuilder(commandStrings);
+		//builder.directory(getWorkingDirectoryFromPreferences());
+		getDefault().getLog().log(new Status(IStatus.INFO,
+		getDefault().getBundle().getSymbolicName(),
+		MessageFormat.format("Starting a new process for command {0} {1} {2}", (Object[])commandStrings)));
+		return builder.start();
+	}
+
+	private static File getWorkingDirectoryFromPreferences() {
+		String wdString = getDefault().getPreferenceStore().getString(P4langPreferenceInitializer.WORKING_DIRECTORY_PREFERENCE);
+		if (wdString == null) {
+			return null;
+		}
+		File wdFile = new File(wdString);
+		if (wdFile.exists() && wdFile.isDirectory()) {
+			return wdFile;
+		}
+		return null;
+	}
+
 }
